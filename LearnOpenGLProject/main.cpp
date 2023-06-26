3e797850f3173fb9c4e2c4fb5963c6fb0d980a59
xzm0307 <1061138927@qq.com>
2023-06-26 21:33:38 +0800

    1

--- LearnOpenGLProject/main.cpp
+++ LearnOpenGLProject/main.cpp
@@ -1,12 +1,34 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main()
 {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // 初始化GLFW
     glfwInit();
+    // 主版本号和次版本号都设置为3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // 使用核心模式
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // MAC OS X
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	return 0;
+	
+    // 创建一个窗口, 宽, 高, 名字
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create CLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    return 0;
 }
